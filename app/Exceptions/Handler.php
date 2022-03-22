a1a2ab888af526921746303a19a80746d65e6b79
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2022-03-22 13:17:24 -0430

    Manejar excepciones

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,13 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
+use Illuminate\Database\QueryException;
+
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -38,4 +45,59 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    protected function invalidJson($request, ValidationException $exception)
+    {
+        return response()->json([
+            'message' => __('Los datos proporcionados no son validos.'),
+            'errors' => $exception->errors(),
+        ], $exception->status);
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if($exception instanceof ModelNotFoundException)
+        {
+            return response()->json([
+                'response' => false,
+                'message' => 'Modelo no encontrado'
+            ],400);
+        }
+
+        if($exception instanceof RouteNotFoundException)
+        {
+            return response()->json([
+                'response' => false,
+                'message' => 'No tienes permiso para acceder a esta ruta'
+            ],401);
+        }
+
+        if($exception instanceof MethodNotAllowedException)
+        {
+            return response()->json([
+                'response' => false,
+                'message' => 'Método HTTP incorrecto durante el enrutamiento de solicitudes'
+            ],400);
+        }
+
+        if($exception instanceof HttpResponseException)
+        {
+            return response()->json([
+                'response' => false,
+                'message' => 'Consulta HTTP sin exito'
+            ],400);
+        }
+
+        if($exception instanceof QueryException)
+        {
+            return response()->json([
+                'response' => false,
+                'message' => 'Error en la ejecución de sentencias SQL'
+            ],400);
+        }
+
+
+        return Parent::render($request, $exception);
+    }
+
 }
