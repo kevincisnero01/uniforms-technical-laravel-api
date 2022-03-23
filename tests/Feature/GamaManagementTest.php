838f9ea59440b86c8d172cc1dcb8156b9d9d06c2
Kevin Cisnero <7884578+kevincisnero01@users.noreply.github.com>
2022-03-23 11:41:29 -0430

    Test Gama II
    -a_gama_can_be_retrieved
    -a_gama_can_be_updated
    -a_gama_can_be_deleted

--- tests/Feature/GamaManagementTest.php
+++ tests/Feature/GamaManagementTest.php
@@ -62,4 +62,82 @@ class GamaManagementTest extends TestCase
         ])->assertStatus(200);
     }
 
+    /** @test */
+    public function a_gama_can_be_retrieved()
+    {
+        $this->withoutExceptionHandling();
+
+        $gama = Gama::factory()->create();
+
+        $response = $this->get('/api/gamas/' . $gama->id_gama);
+        
+        $response->assertOk(200);
+        $response->assertJsonStructure([
+            "data" => [
+                "gama",
+                "id_region",
+                "descripcion",
+                "escudo"
+            ]
+        ]);
+
+        $this->assertDatabasehas('gamas',[
+            "gama" => $gama->gama,
+            "id_region" => $gama->id_region,
+            "descripcion" => $gama->descripcion,
+            "escudo" => $gama->escudo
+        ]);
+    }
+
+    /** @test */
+    public function a_gama_can_be_updated()
+    {
+
+        $this->withoutExceptionHandling();
+
+        $gama = Gama::factory()->create();
+
+        $response = $this->json('PUT' , '/api/gamas/' . $gama->id_gama, [
+            "gama" => "CATALOGO POLICIAL EDIT",
+            "id_region" => "1",
+            "descripcion" => "DESCRIPCIÓN DE CATALOGO EDIT",
+            "escudo" => "ZZZ"
+        ]);
+
+        $response->assertJsonStructure([
+            "data" => [
+                "gama",
+                "id_region",
+                "descripcion",
+                "escudo"
+            ]
+        ])->assertStatus(200);
+
+        $this->assertDatabasehas('gamas',[
+            "gama" => "CATALOGO POLICIAL EDIT",
+            "id_region" => "1",
+            "descripcion" => "DESCRIPCIÓN DE CATALOGO EDIT",
+            "escudo" => "ZZZ"
+        ]);
+    }
+
+    /** @test */
+    public function a_gama_can_be_deleted()
+    {
+
+        $this->withoutExceptionHandling();
+
+        $gama = Gama::factory()->create();
+
+        $response = $this->json('DELETE' , '/api/gamas/' . $gama->id_gama);
+
+        $response->assertStatus(200);
+
+        $this->assertDatabaseMissing('gamas',[
+            "gama" => $gama->gama,
+            "id_region" => $gama->id_region,
+            "descripcion" => $gama->descripcion,
+            "escudo" => $gama->escudo
+        ]);
+    }
 }
